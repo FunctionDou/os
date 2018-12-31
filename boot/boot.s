b943585a14957e19f7ec68ec107f1f161371ec33
0x0001 <1753214784@qq.com>
2018-12-31 12:25:46 +0800

    增加了boot.s的注释和修改

--- boot/boot.s
+++ boot/boot.s
@@ -1,9 +1,3 @@
-; ----------------------------------------------------------------
-;
-;   boot.s -- 内核从这里开始
-;
-; ----------------------------------------------------------------
-
 ; Multiboot 魔数，由规范决定的
 MBOOT_HEADER_MAGIC  equ     0x1BADB002
 
@@ -22,18 +16,10 @@ MBOOT_HEADER_FLAGS  equ     MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
 MBOOT_CHECKSUM      equ     -(MBOOT_HEADER_MAGIC+MBOOT_HEADER_FLAGS)
 
 ; 符合Multiboot规范的 OS 映象需要这样一个 magic Multiboot 头
-; Multiboot 头的分布必须如下表所示：
-; ----------------------------------------------------------
 ; 偏移量  类型  域名        备注
-;
 ;   0     u32   magic       必需
 ;   4     u32   flags       必需 
 ;   8     u32   checksum    必需 
-;
-; 我们只使用到这些就够了，更多的详细说明请参阅 GNU 相关文档
-;-----------------------------------------------------------
-
-;-----------------------------------------------------------------------------
 
 [BITS 32]   ; 所有代码以 32-bit 的方式编译
 section .init.text   ; 代码段从这里开始, 修改 : 开启分页后虚拟地址, kernel.dll重新修改代码段位置
@@ -44,28 +30,28 @@ dd MBOOT_HEADER_MAGIC   ; GRUB 会通过这个魔数判断该映像是否支持
 dd MBOOT_HEADER_FLAGS   ; GRUB 的一些加载时选项，其详细注释在定义处
 dd MBOOT_CHECKSUM       ; 检测数值，其含义在定义处
 
-[GLOBAL start]      ; 向外部声明内核代码入口，此处提供该声明给链接器
+[GLOBAL start]          ; 向外部声明内核代码入口，此处提供该声明给链接器
 [GLOBAL mboot_ptr_tmp]  ; 向外部声明 struct multiboot * 变量 在 <start> 开始的时候, 执行 mov DWORD PTR ds: 0x10a00c ebx语句时, 将ebx指向的地址存给multiboot_t结构体中, 保存全局变量
 [EXTERN kern_entry]     ; 声明内核 C 代码的入口函数, call <kern_entry>
 
 start:
-    cli              ; 此时还没有设置好保护模式的中断处理，要关闭中断
-                 ; 所以必须关闭中断
-    mov esp, STACK_TOP       ; 设置内核栈地址
-    mov ebp, 0       ; 帧指针修改为 0
-    and esp, 0FFFFFFF0H  ; 栈地址按照16字节对齐
+    cli                 ; 此时还没有设置好保护模式的中断处理，要关闭中断
+                        ; 所以必须关闭中断
+    mov esp, STACK_TOP  ; 设置内核栈地址
+    mov ebp, 0          ; 帧指针修改为 0
+    and esp, 0FFFFFFF0H ; 栈地址按照16字节对齐
     mov [mboot_ptr_tmp], ebx ; 将 ebx 中存储的指针存入全局变量, 存放了multiboot_t结构体的指针
     call kern_entry      ; 调用内核入口函数
 ;stop:
-;    hlt              ; 停机指令，可以降低 CPU 功耗
-;    jmp stop         ; 到这里结束，关机什么的后面再说
+;    hlt                ; 停机指令，可以降低 CPU 功耗
+;    jmp stop           ; 到这里结束，关机什么的后面再说
 
 ;-----------------------------------------------------------------------------
 
-;section .bss             ; 未初始化的数据段从这里开始
+;section .bss           ; 未初始化的数据段从这里开始
 ;stack:
-;    resb 32768       ; 这里作为内核栈
-;glb_mboot_ptr:           ; 全局的 multiboot 结构体指针
+;    resb 32768         ; 这里作为内核栈
+;glb_mboot_ptr:         ; 全局的 multiboot 结构体指针
 ;    resb 4
 
 ;STACK_TOP equ $-stack-1      ; 内核栈顶，$ 符指代是当前地址
